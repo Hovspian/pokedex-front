8ad60b3c76eca08e0221dd273eae198976e8e071
Daniel Waters <danwater1@gmail.com>
2018-10-29 13:30:41 -0700

    Adds tests for generic pokemon type component

--- src/__tests__/core/PokemonType.js
+++ src/__tests__/core/PokemonType.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import PokemonType from '../../js/core/PokemonType';
 import ExampleJSON from '../../../local_data/example_single.json';
+import ExampleDetailJSON from '../../../local_data/example_detail.json';
 
-describe('PokemonType', () => {
+
+describe('PokemonType render', () => {
   it('should render Type component without crashing', () => {
-    shallow(<PokemonType id={ExampleJSON.id} types={ExampleJSON.types} />);
+    shallow(<PokemonType isModal isWeakness types={ExampleJSON.types} />);
   });
 
   it('should match Type component snapshot', () => {
-    const wrapper = shallow(<PokemonType id={ExampleJSON.id} types={ExampleJSON.types} />);
+    const wrapper = shallow(<PokemonType isModal isWeakness types={ExampleJSON.types} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
+
+describe('Tests render output for different types and sizes', () => {
+  it('should render 4 types', () => {
+    const wrapper = shallow(<PokemonType isModal isWeakness types={ExampleDetailJSON.forms[0].weaknesses} />);
+    expect(wrapper.children().length).toEqual(ExampleDetailJSON.forms[0].weaknesses.length);
+  });
+
+  it('should render a hover over', () => {
+    const wrapper = shallow(<PokemonType isModal isWeakness types={ExampleDetailJSON.forms[0].weaknesses} />);
+
+    expect(wrapper.exists('.extra-damage-tooltip')).toEqual(true);
+  });
+
+  it('should not render a hover over', () => {
+    const example = Object.assign({}, ExampleDetailJSON);
+    // remove x4 damage multiplier
+    example.forms[0].weaknesses.splice(-1, 1);
+    const wrapper = shallow(<PokemonType isModal isWeakness types={example.forms[0].weaknesses} />);
+    expect(wrapper.exists('.extra-damage-tooltip')).toEqual(false);
+  });
+
+  it('should render a small type', () => {
+    const wrapper = shallow(<PokemonType isModal={false} isWeakness types={ExampleDetailJSON.forms[0].types} />);
+    expect(wrapper.exists('h3')).toEqual(false);
+  });
+
+  it('should render a large type', () => {
+    const wrapper = shallow(<PokemonType isModal isWeakness={false} types={ExampleDetailJSON.forms[0].types} />);
+    expect(wrapper.exists('h3')).toEqual(true);
+  })
+
+})
