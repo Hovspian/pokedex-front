5f73706453817764f6b303a6ff9b8a9409f6c413
Hovsep <hovseplalikian@gmail.com>
2018-11-01 16:51:04 -0700

    Added TypeContainer tests

---
+++ src/__tests__/modal/TypeContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TypeContainer from '../../js/modal/TypeContainer';
+
+import ExampleJSON from '../../../local_data/example_detail.json';
+
+describe('TypeContainer Component', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      types: ExampleJSON.forms[0].types.types,
+      weaknesses: ExampleJSON.forms[0].types.weaknesses,
+    };
+  });
+
+  it('should render TypeContainer component without crashing', () => {
+    shallow(<TypeContainer {...props} />);
+  });
+
+  it('should match TypeContainer component snapshot', () => {
+    const wrapper = shallow(<TypeContainer {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
