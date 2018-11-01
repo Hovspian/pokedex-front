67e64c9f7fc324aa5b3ac4949ec6bc963a98bb09
Hovsep <hovseplalikian@gmail.com>
2018-11-01 16:44:07 -0700

    Added Abilities tests

---
+++ src/__tests__/modal/Abilities.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Abilities from '../../js/modal/Abilities';
+
+import ExampleJSON from '../../../local_data/example_detail.json';
+
+describe('Abilities Component', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      abilities: ExampleJSON.forms[0].abilities,
+    };
+  });
+
+  it('should render Abilities component without crashing', () => {
+    shallow(<Abilities {...props} />);
+  });
+
+  it('should match Abilities component snapshot', () => {
+    const wrapper = shallow(<Abilities {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
