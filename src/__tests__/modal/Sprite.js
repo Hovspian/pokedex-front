c227aad7890019db6f7734dc19aec657e36e6d9a
Hovsep <hovseplalikian@gmail.com>
2018-11-01 16:48:22 -0700

    Added Sprite tests

---
+++ src/__tests__/modal/Sprite.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Sprite from '../../js/modal/Sprite';
+
+import ExampleJSON from '../../../local_data/example_detail.json';
+
+describe('Sprite Component', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      name: ExampleJSON.name,
+      image_path: ExampleJSON.forms[0].image_path,
+      types: ExampleJSON.forms[0].types,
+    };
+  });
+
+  it('should render Sprite component without crashing', () => {
+    shallow(<Sprite {...props} />);
+  });
+
+  it('should match Sprite component snapshot', () => {
+    const wrapper = shallow(<Sprite {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
