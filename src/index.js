f7121a251e960231fea8b306f5132d0d0a373e84
Hovsep <hovseplalikian@gmail.com>
2018-11-06 10:30:28 -0800

    Use registerServiceWorker again

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Home from './js/home/Home';
 
 import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
 // Commented out server worker while working on file locally to
 // keep from caching changes.
-// registerServiceWorker();
+registerServiceWorker();
