a0ebf9d1d54623654647bab5bdaefa5258fd9147
Daniel Waters <danwater1@gmail.com>
2018-11-13 10:26:58 -0800

    Modifies API to handle JSON returned errors and fetch system errors

--- src/js/core/PokemonAPI.js
+++ src/js/core/PokemonAPI.js
@@ -2,11 +2,21 @@ const baseURL = 'https://pokedex-backend-server.herokuapp.com';
 
 export const getRangeOfPokemon = (id, range) =>
   fetch(`${baseURL}/pokemon?id=${id}&range=${range}`)
-    .then(res => res.json())
+    .then(handleResponse)
     .then(json => json.pokemon);
 
 export const getPokemonDetails = (id) =>
   fetch(`${baseURL}/pokemon/${id}`)
-    .then(res => res.json());
+    .then(handleResponse);
 
 export const getPokemonSprite = (image_path) => baseURL + image_path;
+
+const handleResponse = (response) => {
+  return response.json()
+    .then((json) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }    
+      return json;
+    });
+};
