1e7baa388a0272cf744fd6703e7fdc0ca10a929c
Daniel Waters <danwater1@gmail.com>
2018-11-15 21:04:27 -0800

    Adds empty search component

---
+++ src/js/home/EmptySearch.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+import '../../styles/home/EmptySearch.css';
+
+class EmptySearch extends React.Component {
+    
+  render () {
+    return (
+      <Container className="empty-search-container">
+        <h4>No Pokemon Matched Your Search!</h4>
+        <p>Try these suggestions to find a Pokemon</p>
+        <ul>
+            <li>Reduce the number of search parameters</li>
+            <li>Search for only one or two Pokemon types at a time</li>
+            <li>Ensure Number search is within the range</li>
+        </ul>
+      </Container>
+    );
+  }
+}
+
+export default EmptySearch;
