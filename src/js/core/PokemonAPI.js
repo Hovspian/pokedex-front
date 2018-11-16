cd3ebb73b1bae1e040352fc87920d64bf25d8670
Daniel Waters <danwater1@gmail.com>
2018-11-15 21:05:08 -0800

    Refactors PokemonAPI to allow for more search options

--- src/js/core/PokemonAPI.js
+++ src/js/core/PokemonAPI.js
@@ -1,9 +1,19 @@
 const baseURL = 'https://pokedex-backend-server.herokuapp.com';
 
-export const getRangeOfPokemon = (id, range) =>
-  fetch(`${baseURL}/pokemon?id=${id}&range=${range}`)
+export const getRangeOfPokemon = (searchOptions) => {
+  let url = baseURL + '/pokemon';
+  url += Object.keys(searchOptions).map((key, index) => {
+    if (index === 0) {
+      return `?${key}=${searchOptions[key]}`
+    } else {
+      return `&${key}=${searchOptions[key]}`
+    }
+  }).join('');
+
+  return fetch(url)
     .then(handleResponse)
     .then(json => json.pokemon);
+};
 
 export const getPokemonDetails = (id) =>
   fetch(`${baseURL}/pokemon/${id}`)
@@ -16,7 +26,7 @@ const handleResponse = (response) => {
     .then((json) => {
       if (!response.ok) {
         return Promise.reject(json);
-      }    
+      }
       return json;
     });
 };
