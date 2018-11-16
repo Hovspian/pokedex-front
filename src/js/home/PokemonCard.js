09398046ae7ce2b732fa42cc045cd484ece51a37
Daniel Waters <danwater1@gmail.com>
2018-11-15 21:09:13 -0800

    Removes uppercase function since names are already formatted correctly now

--- src/js/home/PokemonCard.js
+++ src/js/home/PokemonCard.js
@@ -16,37 +16,13 @@ const propTypes = {
 
 class PokemonCard extends React.Component {
 
-  /**
-   * Capitalizes a name.
-   * The first letter should be capitalized, as well as any letter following a space or dash
-   * @param  {String} name - the name to capitalize
-   * @return {String} - the capitalized name
-   */
-  capitalizeName (name) {
-    // Capitalize first letter
-    let capitalizedLetter = name.charAt(0).toUpperCase();
-    let capitalizedName = capitalizedLetter + name.slice(1);
-
-    // Capitalize other letters that should be capitalized.
-    let i = capitalizedName.search(/[- ]/);
-    while (i > -1) {
-      capitalizedLetter = capitalizedName.charAt(i + 1).toUpperCase();
-      capitalizedName = capitalizedName.slice(0, i + 1) + capitalizedLetter + capitalizedName.slice(i + 2);
-
-      let next = capitalizedName.substring(i + 1).search(/[- ]/);
-      i = next > -1 ? i + 1 + next : -1;
-    }
-
-    return capitalizedName;
-  }
-
   render () {
     return (
       <div className='card' onClick={() => {this.props.handlePokemonCardClick(this.props.id)}} >
         <img className="card-sprite" src={getPokemonSprite(this.props.image_path)} alt={`${this.props.name} sprite`} />
         <div className="textarea" align="center" >
           <h5 className="pokemon-id" align="left">{`#${this.props.id}`}</h5>
-          <h3><b>{this.capitalizeName(this.props.name)}</b></h3>
+          <h3><b>{this.props.name}</b></h3>
           <PokemonType isModal={false} isWeakness={false} id={this.props.id} types= {this.props.types} />
         </div>
       </div>
